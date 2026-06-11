c55fd1602790c9ae0ece02549eb49a4af401f829
themoinuddin <moinuddin.digital@email.com>
2026-06-11 19:54:42 +0500

    Add variables example

---
+++ 01_basics/variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 19;
+    string name = "Moin";
+    
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    
+    return 0;
+}
