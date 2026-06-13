3456bf89425b7f776871d4c6948234201fd54598
themoinuddin <moinuddin.digital@gmail.com>
2026-06-13 21:49:43 +0500

    Add loops.cpp - for loop basics

---
+++ 01_basics/loops.cpp
@@ -0,0 +1,12 @@
+// loops.cpp
+// Author: Moin Uddin
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        cout << i << endl;
+    }
+    return 0;
+}
