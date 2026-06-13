07402c1bf87568d35f38a9c699d95a62b5b3795e
themoinuddin <moinuddin.digital@gmail.com>
2026-06-13 22:00:53 +0500

    Add while_loop.cpp - while loop basics

---
+++ 01_basics/while_loop.cpp
@@ -0,0 +1,14 @@
+// while_loop.cpp
+// Author: Moin Uddin
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    while (i <= 5) {
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
