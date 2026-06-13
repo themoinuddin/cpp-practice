a6318cb91ea9f09feb1ea4a36dd5a0df49d62ade
themoinuddin <moinuddin.digital@gmail.com>
2026-06-13 23:28:28 +0500

    Add nested_loops.cpp - nested for loops

---
+++ 01_basics/nested_loops.cpp
@@ -0,0 +1,15 @@
+// nested_loops.cpp
+// Author: Moin Uddin
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 3; j++) {
+            cout << i << " x " << j << " = " << i * j << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
