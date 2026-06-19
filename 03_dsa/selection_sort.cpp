7038996d471e9ae5d7542811b421f9e3f520e27e
themoinuddin <moinuddin.digital@gmail.com>
2026-06-20 01:53:29 +0500

    Add selection_sort.cpp - selection sort algorithm

---
+++ 03_dsa/selection_sort.cpp
@@ -0,0 +1,32 @@
+// selection_sort.cpp
+// Author: Moin Uddin
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int size = 5;
+
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
