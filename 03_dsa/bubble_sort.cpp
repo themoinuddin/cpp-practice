6078bcbf3d73a0479bea898c2b32bfc38a6391b0
themoinuddin <moinuddin.digital@gmail.com>
2026-06-20 01:52:42 +0500

    Add bubble_sort.cpp - bubble sort algorithm

---
+++ 03_dsa/bubble_sort.cpp
@@ -0,0 +1,28 @@
+// bubble_sort.cpp
+// Author: Moin Uddin
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22};
+    int size = 5;
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
