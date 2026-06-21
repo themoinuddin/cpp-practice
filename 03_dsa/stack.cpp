40f47474b9ff44e624c451c61977271e5a634ebc
themoinuddin <moinuddin.digital@gmail.com>
2026-06-21 23:18:46 +0500

    Add stack.cpp - stack data structure

---
+++ 03_dsa/stack.cpp
@@ -0,0 +1,23 @@
+// stack.cpp
+// Author: Moin Uddin
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int> s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Top element: " << s.top() << endl;
+
+    s.pop();
+    cout << "After pop, top: " << s.top() << endl;
+
+    cout << "Size: " << s.size() << endl;
+
+    return 0;
+}
