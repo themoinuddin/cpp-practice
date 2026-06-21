9ef036f590e3b9efccbeff7d8961cc25ad741f5a
themoinuddin <moinuddin.digital@gmail.com>
2026-06-21 23:31:26 +0500

    Add insertion_sort.cpp - insertion sort algorithm

---
+++ 03_dsa/insertion_sort.cpp
@@ -0,0 +1,29 @@
+// insertion_sort.cpp
+// Author: Moin Uddin
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int size = 5;
+
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
